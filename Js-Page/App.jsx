d515658c4404e7e5644577dfef9062f71f7e716c
Ilyass <kreilyass@gmail.com>
2023-10-23 01:16:20 +0100

    if click in number and roll it stays

--- Js-Page/App.jsx
+++ Js-Page/App.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from "react"
 import Dice from "./Components/Dice"
-
+import { nanoid } from 'nanoid'
 
 
 function App() {
@@ -10,22 +10,59 @@ function App() {
       const randomArrayNumber = []
       for(let i=0 ; i <10 ; i++) {
         const randomNumber = Math.floor(Math.random() * 6 + 1)
-        randomArrayNumber.push(randomNumber)
+        randomArrayNumber.push({
+          value: randomNumber, 
+          isHeld:false,
+          id:nanoid()
+        })
       }
       return randomArrayNumber
-    }
 
+    }
+    
     const [numsDice , setNumsDice] = useState(allNewDice)
-
+    
     function handleClick() {
-      const newArray = allNewDice()
-      setNumsDice(() => {
-        return newArray
-      })
+      setNumsDice(prevNum => {
+        const newNum = prevNum.map(die => {
+          if(die.isHeld) {
+            return die
+          } else {
+            return { 
+              value: Math.floor(Math.random() * 6 + 1), 
+              isHeld:false,
+              id:nanoid()
+            }
+          }
+        })
+        return newNum
+      })  
     }
 
+    function handletoggle(id) {
+      setNumsDice(prevDice => {
+        
+        const newDice = prevDice.map(die => {
+          if (die.id === id) {
+            return {
+              ...die,isHeld: !die.isHeld
+            }
+          } else {
+            return die
+          }
+          
+        })
+        return newDice
+      })
+      
+    }
+    
     const diceNumberd = numsDice.map((numDice) => {
-        return <Dice key={numDice === numDice ? numDice*Math.random() : numDice} number={numDice} />
+        return <Dice 
+            key={numDice.id}
+            number={numDice.value}
+            isHeld={numDice.isHeld}
+            toggle={() => handletoggle(numDice.id)} />
     })
 
     
--- Js-Page/Components/Dice.jsx
+++ Js-Page/Components/Dice.jsx
@@ -1,9 +1,12 @@
 
 
 function Dice(props) {
-
+    const styles= {
+        backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
+    }
+    
     return (
-        <div  className="dice-div">
+        <div  className="dice-div" onClick={props.toggle} style={styles}>
             <h2 className="dice-num">{props.number}</h2>
         </div>
     )
